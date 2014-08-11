9bf328fc2d9674f6d6c6b74fb1ccd5f909a7a917
Cornelius Adams <cornelius.adams@conlabz.de>
2014-08-11 12:17:57 +0200

    composer.json autoloader

--- Module.php
+++ Module.php
@@ -1,70 +1,7 @@
 <?php
-namespace ConLayout;
-
-use Zend\EventManager\EventInterface as Event,
-    Zend\Mvc\MvcEvent;
 /**
- * ConLayout\Module
- * 
- * 
+ * This file is placed here for compatibility with ZendFramework 2's ModuleManager.
+ * It allows usage of this module even without composer.
+ * The original Module.php is in 'src/BjyAuthorize' in order to respect PSR-0
  */
-class Module
-{
-    /**
-     * retrieve module config
-     * 
-     * @return array
-     */
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-    
-    /**
-     * retrieve services
-     * 
-     * @return array
-     */
-    public function getServiceConfig()
-    {
-        return include __DIR__ . '/config/service.config.php';
-    }
-    
-    /**
-     * 
-     * @param \Zend\EventManager\EventInterface $e
-     */
-    public function onBootstrap(Event $e)
-    {
-        $application    = $e->getApplication();
-        $serviceManager = $application->getServiceManager();        
-        $eventManager   = $application->getEventManager();
-        
-        $eventManager->attach(MvcEvent::EVENT_RENDER, function($e) use ($serviceManager) {
-            $layoutModifier = $serviceManager->get('ConLayout\Service\LayoutModifier');
-            $layoutModifier->addBlocksToLayout();
-        }); 
-        
-        $eventManager->attach($serviceManager->get('ConLayout\Listener\ActionHandles'));
-        
-        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, function($e) use ($serviceManager) {
-            $serviceManager->get('ConLayout\Service\LayoutService')
-                ->addHandle($e->getError());
-        }, 100);
-    }
-
-    /**
-     * 
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+require_once __DIR__ . '/src/ConLayout/Module.php';
---
+++ src/ConLayout/Module.php
@@ -0,0 +1,70 @@
+<?php
+namespace ConLayout;
+
+use Zend\EventManager\EventInterface as Event,
+    Zend\Mvc\MvcEvent;
+/**
+ * ConLayout\Module
+ * 
+ * 
+ */
+class Module
+{
+    /**
+     * retrieve module config
+     * 
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+    
+    /**
+     * retrieve services
+     * 
+     * @return array
+     */
+    public function getServiceConfig()
+    {
+        return include __DIR__ . '/../../config/service.config.php';
+    }
+    
+    /**
+     * 
+     * @param \Zend\EventManager\EventInterface $e
+     */
+    public function onBootstrap(Event $e)
+    {
+        $application    = $e->getApplication();
+        $serviceManager = $application->getServiceManager();        
+        $eventManager   = $application->getEventManager();
+        
+        $eventManager->attach(MvcEvent::EVENT_RENDER, function($e) use ($serviceManager) {
+            $layoutModifier = $serviceManager->get('ConLayout\Service\LayoutModifier');
+            $layoutModifier->addBlocksToLayout();
+        }); 
+        
+        $eventManager->attach($serviceManager->get('ConLayout\Listener\ActionHandles'));
+        
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, function($e) use ($serviceManager) {
+            $serviceManager->get('ConLayout\Service\LayoutService')
+                ->addHandle($e->getError());
+        }, 100);
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
